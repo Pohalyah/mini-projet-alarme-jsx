7f11f0c01a6f7e6074b95285ce42a56f74c7f745
Pohalyah <josua.calassou@orange.fr>
2026-06-23 11:11:09 +0200

    update timer

--- src/App.jsx
+++ src/App.jsx
@@ -178,14 +178,13 @@ function App() {
                       }
                       return "00:00";
                     }
-
-                    const totalMinutes = Math.floor(remaining / 60000);
+                    const totalSeconds = Math.floor(remaining / 1000)
                     const hours = Math.floor(totalMinutes / 60);
                     const minutes = totalMinutes % 60;
+                    const secondes = totalSeconds % 60
 
-                    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`;
-                  })()
-                  : ""}
+                    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(secondes).padStart(2, "0")}`;
+                  })() : ""}
               </p>
 
               <button
